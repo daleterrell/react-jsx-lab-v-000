8c1dd29417636e05af63db1b6d4c770e36fc92a6
Dale Terrell <tragicid@gmail.com>
2019-05-03 23:23:56 +0000

    Automatically backed up by Learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,4 +1,10 @@
-<Webpage>
-  <title>The world's coolest webpage</title>
-  <./FillerText.js>
-  <./FillerText.js>
+
+import React from 'react';
+
+export default class Webpage extends React.Component {
+  render() {
+    return (
+      <Webpage>
+        <title>The world's coolest webpage</title>
+        <./FillerText.js>
+        <./FillerText.js>
