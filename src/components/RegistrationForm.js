6c07cef8556c5700e960c498460ae9aa6d45f3e2
Dale Terrell <tragicid@gmail.com>
2019-05-03 23:27:21 +0000

    Automatically backed up by Learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -4,9 +4,11 @@ import React from 'react';
 export default class RegistrationForm extends React.Component {
   render() {
     return (
-      <RegistrationForm>
-        <form>
-          <input type="text" value="text" />
-          <input type="password" value="password" />
-          <Button type="submit">Submit</Button>
-          
+      <form>
+        <input type="text" />
+        <input type="password" />
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
